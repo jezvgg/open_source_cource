978254a8754852b92e6d4cfb1b70f9c7f6f7836a
Danil138 <playzerou138@mail.ru>
2025-01-28 12:13:51 +0800

    Add pagination to /items endpoint

--- git_feature_branches/main.py
+++ git_feature_branches/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Query
 
 app = FastAPI()
 
@@ -7,9 +8,10 @@ def read_root():
     return {"message": "Hello, World!"}
 
 @app.get("/items")
-def get_items():
-    return {"items": ["item1", "item2", "item3"]}
+def get_items(skip: int = Query(0), limit: int = Query(10)):
+    all_items = ["item1", "item2", "item3", "item4", "item5"]
+    return {"items": all_items[skip : skip + limit]}
 
 @app.get("/item")
-def get_items():
+def get_item():
     return "item1"
