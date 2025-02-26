25b6febaafba85f80de0ed0d369ec56e6d7ae150
jezv <jezv@yandex.ru>
2025-02-26 22:06:55 +0800

    #12 Done Refactor all project

--- testing_mocks/cli/cli_client.py
+++ testing_mocks/cli/cli_client.py
@@ -1,85 +1,121 @@
+from typing import Callable
+
 import questionary
 import httpx
-import json
 
- # Adjust this to your FastAPI server URL
+from cli.enums.choices import Choice
+
 
 
 
 class CLI_Client:
-    funtion: dict
+    '''
+    Класс реализующий логику CLI.
+    Основан на фреймворке questionary и реализует логику взаимодействия с сервервенрой частью.
+
+    :param BASE_URL: Константное значение адреса по которому обращаемся
+    :param funtion: Словарь взаимодействий
+    '''
     BASE_URL = "http://localhost:8000" 
+    funtion: dict[Choice, Callable]
+
 
     def __init__(self):
         self.funtion = {
-                "Register a new user": self.register_user,
-                "Add an item for a user": self.add_item,
-                "Get an item for a user": self.get_item,
-                "Get all users": self.get_all_usrs
+                Choice.REGISTER: self.register_user,
+                Choice.ADD_ITEM: self.add_item,
+                Choice.GET_ITEM: self.get_item,
+                Choice.GET_USERS: self.get_all_usrs
             }
         
 
-    def start_client(self):
+    def start_client(self) -> None:
+        '''
+        Функция для запуска клиента.
+        Не ну тут просто угар, ansver, funtion XD
+        '''
         action = self.ask()
-        while action != "Exit":
+        while action != Choice.EXIT:
             ansver = self.funtion[action]()
-            print(ansver)
+            print(ansver) # Будем думать, что это логгирование, в малом приложении
             action = self.ask()   
 
 
     
-    def ask(self):
+    def ask(self) -> Choice:
+        '''
+        Функция реализующая вопрошание. В зависимости от ответа вызывает другие функции из перечня.
+        '''
         action = questionary.select(
             "What would you like to do?",
             choices=[
-                "Register a new user",
-                "Get all users"
-                "Add an item for a user",
-                "Get an item for a user",
-                "Exit"
+                Choice.REGISTER,
+                Choice.GET_USERS,
+                Choice.ADD_ITEM,
+                Choice.GET_ITEM,
+                Choice.EXIT
             ]
         ).ask()
         return action
     
-    def get_all_usrs(self):
+
+    def get_all_usrs(self) -> str:
+        '''
+        Функция, реализующая логику, получения всех пользователей.
+        '''
         response = httpx.get(f"{self.BASE_URL}/")
         users = response.json()
-        if response.status_code == 200:
-            if len(users)!=0:
-                return f"Users: {users}"
-            return "No users"
-        return "Failed to get alluser."
-    
+
+        if response.status_code != 200: return "Failed to get alluser."
+        if len(users)==0:return "No users"
+            
+        return f"Users: {users}"
 
     
-    def register_user(self,username=None):
-        if username==None:
-            username = questionary.text("Enter username:").ask()
+    def register_user(self, username: str | None = None) -> str:
+        '''
+        Функция реализующая регистрацию пользователя.
+
+        :param username: Имя пользователя default to None
+        '''
+        if not username: username = questionary.text("Enter username:").ask()
         response = httpx.post(f"{self.BASE_URL}/register/{username}")
-        if response.status_code == 200:
-            return "User registered successfully!"
-        return "Failed to register user."
+
+        if response.status_code != 200: return "Failed to register user."
+        return "User registered successfully!"
+
     
-    def add_item(self,username=None,file_path=None):
-        if username==None:
-            username = questionary.text("Enter username:").ask()
-        if file_path==None:
-            file_path = questionary.text("Enter the path to the file:").ask()
+    def add_item(self, username: str | None = None, file_path: str | None = None) -> str:
+        '''
+        Функция добавления csv файла к пользователю.
+
+        :param username: Имя пользователя default to None
+        :param file_path: Путь до файла в виде строки default to None
+        '''
+        if not username: username = questionary.text("Enter username:").ask()
+        if not file_path: file_path = questionary.text("Enter the path to the file:").ask()
+
         with open(file_path, "rb") as file:
             response = httpx.post(f"{self.BASE_URL}/add/{username}", files={"file": file})
-            if response.status_code == 200:
-                return "Item added successfully!"
-            return f"Failed to add item: {response.json().get('detail')}"
+            if response.status_code != 200: 
+                return f"Failed to add item: {response.json().get('detail')}"
+            
+            return "Item added successfully!"
     
 
-    def get_item(self,username=None):
-        if username==None:
-            username = questionary.text("Enter username:").ask()
+    def get_item(self, username: str | None = None) -> tuple[str, str]:
+        '''
+        Функция получения файла привязанного к пользователю.
+
+        :param username: Имя пользователя default to None
+        '''
+        if not username: username = questionary.text("Enter username:").ask()
+
         response = httpx.get(f"{self.BASE_URL}/get/{username}")
-        if response.status_code == 200:
-            item_data = response.json()
-            return "Item data:", json.dumps(item_data, indent=2)
-        return f"Failed to get item: {response.json().get('detail')}"
+        if response.status_code != 200:
+            return f"Failed to get item: {response.json().get('detail')}"
+        
+        return "Item data:", response.text
         
 
 
--- testing_mocks/tests/core_test/test_core.py
+++ testing_mocks/tests/core_test/test_core.py
@@ -2,28 +2,25 @@ import unittest
 import json
 
 from core.converter import Converter
-from core.deconverter import Deconverter
 from core.enums import ConvertType
+
+
 
 class TestCore(unittest.TestCase):
 
 
     def test_converter(self):
-        convert = Converter()
         obj = {"data":"data1"}
-        
 
-        result = convert.convert(ConvertType.JSON,obj)
-
+        result = Converter.convert(ConvertType.JSON,obj)
 
         assert json.loads(result) == {"data": "data1"}
 
+
     def test_deconvertor(self):
-        deconverter = Deconverter()
-        obj =  bytes(b'"Header1","Header2"\n"value1",2')
+        obj =  b'"Header1","Header2"\n"value1",2'
 
-
-        result = deconverter.deconvert(ConvertType.CSV,obj)
+        result = Converter.deconvert(ConvertType.CSV, obj)
 
         assert result == {'rows': [{'Header1': 'value1', 'Header2': 2}]}
 
