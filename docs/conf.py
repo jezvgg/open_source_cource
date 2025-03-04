a984bb10edc9379515a24767b81ce08408209b06
Danil138 <playzerou138@mail.ru>
2025-03-04 23:49:09 +0800

    #11 Fix Add docs for read_the_docs. Add config files read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../testing_mocks'))  
+
+sys.path.insert(0, os.path.abspath('../testing_mocks'))
+
+project = 'Testing mocks'
+copyright = '2025, Danil Gorbachevsckiy, Georgiy Volovikov'
+author = 'Danil Gorbachevsckiy, Georgiy Volovikov'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-project = 'Teasting mocks'
-author = 'Volovicov, Gorbachevskiy'
-release = '1.0.0'
 
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon', 
-]
-
-
-
-html_theme = 'sphinx_rtd_theme' 
-
-html_title = 'Документация Teasting mocks'
-
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/core.enums.rst
@@ -0,0 +1,21 @@
+core.enums package
+==================
+
+Submodules
+----------
+
+core.enums.convert\_type module
+-------------------------------
+
+.. automodule:: core.enums.convert_type
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: core.enums
+   :members:
+   :show-inheritance:
+   :undoc-members:
